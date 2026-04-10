9f9f1bc6907c19df1b9d65f97514e8f36f77afd4
Julius <jukrber@gmx.de>
2026-04-10 12:09:51 +0200

    Stabile Version mit Stufe 3

--- src/main/kotlin/de/thws/fiw/kotlin/mygrep/mygrep.kt
+++ src/main/kotlin/de/thws/fiw/kotlin/mygrep/mygrep.kt
@@ -2,26 +2,58 @@ package de.thws.fiw.kotlin.mygrep
 
 import kotlinx.cli.*
 import java.io.File
+import java.io.IOException
+import kotlin.system.exitProcess
 import kotlin.text.endsWith
+
+enum class Target
+{
+    NONE,
+    FILE,
+    DIRECTORY,
+    BOTH
+}
 
 data class Settings(
     val ignoreCase: Boolean,
     val printLineNumbers: Boolean,
     val invertSearch: Boolean,
     val pattern: String,
-    val directoryName : String,
-    val fileName: String?
+    val fileName: String,
+    val directoryName: String,
+    val target : Target
+)
+
+enum class FileStatus
+{
+    SUCCESS,
+    NOT_FOUND,
+    NOT_READ,
+}
+
+enum class DirectoryStatus
+{
+    SUCCESS,
+    NOT_FOUND,
+    NOT_READ,
+}
+
+data class FileResult(
+    val file : File,
+    val lines: List<LineInfo>,
+    val status : FileStatus
+)
+
+data class DirectoryResult(
+    val fileResults : List<FileResult>,
+    val errorFileResult : FileResult?,
+    val status : DirectoryStatus
 )
 
 data class LineInfo(
     val content : String,
     val number : Int,
     val fileName : String
-)
-
-data class ReadResult(
-    val lines: List<LineInfo>,
-    val status : Boolean?
 )
 
 fun parse(args: Array<String>): Settings
@@ -49,99 +81,149 @@ fun parse(args: Array<String>): Settings
         description = "Invertierte Suche"
     ).default(false)
 
+    val file = parser.option(
+        type = ArgType.String,
+        fullName = "file",
+        shortName = "f",
+        description = "Text-Datei zum Durchsuchen"
+    ).default("")
+
+    val directory = parser.option(
+        type = ArgType.String,
+        fullName = "directory",
+        shortName = "d",
+        description = "Ziel-Ordner (nur .txt-Dateien werden durchsucht)"
+    ).default("")
+
     val pattern = parser.argument(
         type = ArgType.String,
         fullName = "pattern",
         description = "Suchmuster zum Durchsuchen von Text-Dateien"
     )
 
-    val fileOrDirectoryName = parser.argument(
-        type = ArgType.String,
-        fullName = "fileinfo",
-        description = "Datei(.txt) oder Ordner zum Durchsuchen",
-    )
-
     parser.parse(args)
 
-    val directoryName = if (fileOrDirectoryName.value.endsWith(".txt")) "." else fileOrDirectoryName.value
-    val fileName = if (fileOrDirectoryName.value.endsWith(".txt")) fileOrDirectoryName.value else null
+    val enteredFile = file.value.isNotEmpty()
+    val enteredDirectory = directory.value.isNotEmpty()
+    val target = when {
+        enteredFile && enteredDirectory -> Target.BOTH
+        enteredFile && !enteredDirectory -> Target.FILE
+        !enteredFile && enteredDirectory -> Target.DIRECTORY
+        else -> Target.NONE
+    }
 
-    return Settings(ignoreCase.value, printLineNumbers.value, invertSearch.value,
-        pattern.value, directoryName, fileName)
+    return Settings(ignoreCase.value, printLineNumbers.value, invertSearch.value, pattern.value,
+        file.value, directory.value, target)
 }
 
-fun readFiles(settings: Settings) : ReadResult
+fun readFiles(settings: Settings) : DirectoryResult
 {
-    val lines = mutableListOf<LineInfo>()
+    if(settings.target == Target.FILE)
+    {
+        val file = File(settings.fileName)
+        val fileResult = readFile(file)
+        val errorFileResult = if (fileResult.status == FileStatus.SUCCESS) null else fileResult
+        return DirectoryResult( listOf(fileResult), errorFileResult, DirectoryStatus.SUCCESS)
+    }
 
     val directory = File(settings.directoryName)
-    if (!directory.exists() || !directory.isDirectory) return ReadResult(emptyList(), null)
+    if (!directory.exists() || !directory.isDirectory)
+        return DirectoryResult(emptyList(), null, DirectoryStatus.NOT_FOUND)
 
-    val textFiles =
-        if (settings.fileName == null) directory.listFiles{_, name -> name.endsWith(".txt")}
-        else directory.listFiles{_, name -> name.equals(settings.fileName)}
+    val textFiles = directory.listFiles{_, name -> name.endsWith(".txt")}
+    if(textFiles == null)
+        return DirectoryResult(emptyList(), null, DirectoryStatus.NOT_READ)
 
-    if(textFiles == null) return ReadResult(emptyList(), null)
-    if(textFiles.isEmpty()) return ReadResult(emptyList(), false)
-
+    val results = mutableListOf<FileResult>()
     for(file in textFiles)
     {
-        lines.addAll(read(file))
+        val result = readFile(file)
+        results.add(result)
+        if(result.status != FileStatus.SUCCESS)
+        {
+            return DirectoryResult(results, result, DirectoryStatus.SUCCESS)
+        }
     }
-    return ReadResult(lines, true)
+    return DirectoryResult(results, null, DirectoryStatus.SUCCESS)
 }
 
-fun read(file : File) : List<LineInfo>
+fun readFile(file : File) : FileResult
 {
     val lineInfos = mutableListOf<LineInfo>()
-    file.readLines().forEachIndexed { index, line ->
-        lineInfos.add(LineInfo(line, index + 1, file.name))
+
+    if(!file.exists()) return FileResult(file, lineInfos, FileStatus.NOT_FOUND)
+
+    try
+    {
+        file.readLines().forEachIndexed { index, line ->
+            lineInfos.add(LineInfo(line, index + 1, file.name))
+        }
     }
-    return lineInfos
+    catch (_: IOException)
+    {
+        return FileResult(file, lineInfos, FileStatus.NOT_READ)
+    }
+
+    return FileResult(file, lineInfos, FileStatus.SUCCESS)
 }
 
-fun search(lines : List<LineInfo>, settings: Settings) : List<String>
+fun search(results : List<FileResult>, settings: Settings) : List<String>
 {
-    val (ignoreCase, printLineNumbers, invertSearch, pattern) = settings
+    val (ignoreCase, printLineNumbers, invertSearch, pattern, fileName, directoryName, target) = settings
 
     val output = mutableListOf<String>()
-    for(line in lines)
+    for(result in results)
     {
-        if(line.content.contains(pattern, ignoreCase) == !invertSearch)
+        for(line in result.lines)
         {
-            val filePrefix = if (settings.fileName == null) "(${line.fileName}): " else ""
-            val linePrefix = if (printLineNumbers) "|${line.number}| " else ""
-            output.add(filePrefix + linePrefix + line.content)
+            if (line.content.contains(pattern, ignoreCase) == !invertSearch)
+            {
+                val filePrefix = if (target == Target.DIRECTORY) "(${line.fileName}): " else ""
+                val linePrefix = if (printLineNumbers) "|${line.number}| " else ""
+                output.add(filePrefix + linePrefix + line.content)
+            }
         }
     }
     return output
 }
 
-fun main(args: Array<String>)
+fun abort(message : String)
 {
+    System.err.println(message)
+    exitProcess(1)
+}
+
+fun main(args: Array<String>) {
     val settings = parse(args)
-    val readResult = readFiles(settings)
-    if(readResult.status == null)
-    {
-        val error =
-            "Auf den Ordner ${File(settings.directoryName).absolutePath} konnte nicht zugegriffen werden! " +
-                    "Existiert der Ordner?"
-        System.err.println(error)
+    when (settings.target) {
+        Target.NONE ->
+            abort("Es wurde weder die Option -f <Dateiname.txt> noch die Option -d <Ordner> angegeben!")
+        Target.BOTH ->
+            abort("Es wurden beide Optionen -f <Dateiname.txt> und -d <Ordner> angegeben!" +
+                    " Bitte nur eine der Optionen angeben!")
+        else -> {}
     }
-    else if(!readResult.status)
-    {
-        val error =
-            if(settings.fileName == null)
-                "Im Ordner ${File(settings.directoryName).absolutePath} konnten keine Text-Dateien gefunden werden!"
-            else
-                "Die Datei ${File(settings.fileName).name} " +
-                        "im aktuellen Ordner ${File(settings.directoryName).absolutePath} " +
-                        "konnte nicht gefunden werden!"
-        System.err.println(error)
+
+    val result = readFiles(settings)
+    when (result.status) {
+        DirectoryStatus.NOT_FOUND ->
+            abort("Der Ordner ${File(settings.directoryName).absolutePath} " +
+                    "konnte nicht gefunden werden!")
+        DirectoryStatus.NOT_READ ->
+            abort("Auf Den Ordner ${File(settings.directoryName).absolutePath} " +
+                    "konnte nicht zugegriffen werden!")
+        DirectoryStatus.SUCCESS -> {}
     }
-    else
-    {
-        val output = search(readResult.lines, settings)
-        output.forEach(::println)
+
+    val errorFile = result.errorFileResult
+    if (errorFile != null) {
+        when (errorFile.status) {
+            FileStatus.NOT_FOUND -> abort("Die Datei ${errorFile.file.absolutePath} konnte nicht gefunden werden!")
+            FileStatus.NOT_READ -> abort("Auf die Datei ${errorFile.file.absolutePath} konnte nicht zugegriffen werden!")
+            FileStatus.SUCCESS -> {}
+        }
     }
+
+    val output = search(result.fileResults, settings)
+    output.forEach(::println)
 }
---
+++ testfiles/input.c
@@ -0,0 +1,5 @@
+How are you today?
+Hello World
+Hello World
+HELLO WORLD
+Hello Earth
